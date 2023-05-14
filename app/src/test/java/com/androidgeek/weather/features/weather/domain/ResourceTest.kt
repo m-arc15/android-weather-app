cd8e317732b5095a6b896cc93c83395d7b618088
artishevskym <56644443+artishevskym@users.noreply.github.com>
2023-05-14 16:47:05 +0100

    Add wrapper for handling a successful outcome or a failure

---
+++ app/src/test/java/com/androidgeek/weather/features/weather/domain/ResourceTest.kt
@@ -0,0 +1,30 @@
+package com.androidgeek.weather.features.weather.domain
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.assertNull
+import org.junit.jupiter.api.Assertions.assertTrue
+import org.junit.jupiter.api.Test
+
+
+internal class ResourceTest {
+
+    private lateinit var resource: Resource<*>
+
+    @Test
+    fun testSuccessResource() {
+        resource = Resource.Success(100)
+
+        assertTrue(resource is Resource.Success)
+        assertEquals(100, resource.data)
+        assertNull(resource.message)
+    }
+
+    @Test
+    fun testErrorResource() {
+        resource = Resource.Error<Int>("Invalid request")
+
+        assertTrue(resource is Resource.Error)
+        assertNull(resource.data)
+        assertEquals("Invalid request", resource.message)
+    }
+}
