935330ce1067890b98cf67265261e5b8da82da6c
artishevskym <56644443+artishevskym@users.noreply.github.com>
2023-05-20 12:24:06 +0100

    Extend WeatherType

--- app/src/main/java/com/androidgeek/weather/features/weather/domain/model/WeatherType.kt
+++ app/src/main/java/com/androidgeek/weather/features/weather/domain/model/WeatherType.kt
@@ -27,6 +27,31 @@ sealed class WeatherType(
     object ModerateRain : WeatherType("Moderate rain", R.drawable.ic_rainy)
     object HeavyRain : WeatherType("Heavy rain", R.drawable.ic_rainy)
 
+    object LightFreezingRain : WeatherType("Light freezing rain", R.drawable.ic_snowyrainy)
+    object HeavyFreezingRain : WeatherType("Heavy freezing rain", R.drawable.ic_snowyrainy)
+
+    object SlightSnowFall : WeatherType("Slight snow fall", R.drawable.ic_snowy)
+    object ModerateSnowFall : WeatherType("Moderate snow fall", R.drawable.ic_heavysnow)
+    object HeavySnowFall : WeatherType("Heavy snow fall", R.drawable.ic_heavysnow)
+
+    object SnowGrains : WeatherType("Snow grains", R.drawable.ic_heavysnow)
+
+    object SlightRainShowers : WeatherType("Slight rain showers", R.drawable.ic_rainshower)
+    object ModerateRainShowers : WeatherType("Moderate rain showers", R.drawable.ic_rainshower)
+    object ViolentRainShowers : WeatherType("Violent rain showers", R.drawable.ic_rainshower)
+
+    object SlightSnowShowers : WeatherType("Slight snow showers", R.drawable.ic_snowy)
+    object HeavySnowShowers : WeatherType("Heavy snow showers", R.drawable.ic_snowy)
+
+    object ModerateThunderstorm : WeatherType("Moderate thunderstorm", R.drawable.ic_thunder)
+    object SlightHailThunderstorm :
+        WeatherType("Thunderstorm with slight hail", R.drawable.ic_rainythunder)
+
+    object HeavyHailThunderstorm :
+        WeatherType("Thunderstorm with heavy hail", R.drawable.ic_rainythunder)
+
+    object UNREACHABLE : WeatherType("Unreachable weather type", R.drawable.ic_launcher_background)
+
     companion object {
 
         val DEFAULT = ClearSky
@@ -58,33 +83,66 @@ sealed class WeatherType(
                 1 -> MainlyClear
                 2 -> PartlyCloudy
                 3 -> Overcast
-                else -> DEFAULT
+                else -> UNREACHABLE
             }
 
             fun fromFoggy(code: Int) = when (code) {
                 45 -> Foggy
                 48 -> DepositingRimeFog
-                else -> DEFAULT
+                else -> UNREACHABLE
             }
 
             fun fromDrizzle(code: Int) = when (code) {
                 51 -> LightDrizzle
                 53 -> ModerateDrizzle
                 55 -> DenseDrizzle
-                else -> DEFAULT
+                else -> UNREACHABLE
             }
 
-            fun fromFreezingDrizzle(code: Int) = when(code) {
+            fun fromFreezingDrizzle(code: Int) = when (code) {
                 56 -> LightFreezingDrizzle
                 57 -> DenseFreezingDrizzle
-                else -> DEFAULT
+                else -> UNREACHABLE
             }
 
-            fun fromRain(code: Int) = when(code) {
+            fun fromRain(code: Int) = when (code) {
                 61 -> SlightRain
                 63 -> ModerateRain
                 65 -> HeavyRain
-                else -> DEFAULT
+                else -> UNREACHABLE
+            }
+
+            fun fromFreezingRain(code: Int) = when (code) {
+                66 -> LightFreezingRain
+                67 -> HeavyFreezingRain
+                else -> UNREACHABLE
+            }
+
+            fun fromSnowFall(code: Int) = when (code) {
+                71 -> SlightSnowFall
+                73 -> ModerateSnowFall
+                75 -> HeavySnowFall
+                else -> UNREACHABLE
+            }
+
+            fun fromRainShowers(code: Int) = when (code) {
+                80 -> SlightRainShowers
+                81 -> ModerateRainShowers
+                82 -> ViolentRainShowers
+                else -> UNREACHABLE
+            }
+
+            fun fromSnowShowers(code: Int) = when (code) {
+                85 -> SlightSnowShowers
+                86 -> HeavySnowShowers
+                else -> UNREACHABLE
+            }
+
+            fun fromThunderstorm(code: Int) = when (code) {
+                95 -> ModerateThunderstorm
+                96 -> SlightHailThunderstorm
+                99 -> HeavyHailThunderstorm
+                else -> UNREACHABLE
             }
 
             return when (code) {
@@ -94,6 +152,12 @@ sealed class WeatherType(
                 51, 53, 55 -> fromDrizzle(code)
                 56, 57 -> fromFreezingDrizzle(code)
                 61, 63, 65 -> fromRain(code)
+                66, 67 -> fromFreezingRain(code)
+                71, 73, 75 -> fromSnowFall(code)
+                77 -> SnowGrains
+                80, 81, 82 -> fromRainShowers(code)
+                85, 86 -> fromSnowShowers(code)
+                95, 96, 99 -> fromThunderstorm(code)
                 else -> DEFAULT
             }
         }
--- app/src/test/java/com/androidgeek/weather/features/weather/domain/model/WeatherTypeTest.kt
+++ app/src/test/java/com/androidgeek/weather/features/weather/domain/model/WeatherTypeTest.kt
@@ -2,7 +2,36 @@ package com.androidgeek.weather.features.weather.domain.model
 
 import androidx.annotation.DrawableRes
 import com.androidgeek.weather.R
+import com.androidgeek.weather.features.weather.domain.model.WeatherType.ClearSky
+import com.androidgeek.weather.features.weather.domain.model.WeatherType.Companion.DEFAULT
 import com.androidgeek.weather.features.weather.domain.model.WeatherType.Companion.fromWMO
+import com.androidgeek.weather.features.weather.domain.model.WeatherType.DenseDrizzle
+import com.androidgeek.weather.features.weather.domain.model.WeatherType.DenseFreezingDrizzle
+import com.androidgeek.weather.features.weather.domain.model.WeatherType.DepositingRimeFog
+import com.androidgeek.weather.features.weather.domain.model.WeatherType.Foggy
+import com.androidgeek.weather.features.weather.domain.model.WeatherType.HeavyFreezingRain
+import com.androidgeek.weather.features.weather.domain.model.WeatherType.HeavyHailThunderstorm
+import com.androidgeek.weather.features.weather.domain.model.WeatherType.HeavyRain
+import com.androidgeek.weather.features.weather.domain.model.WeatherType.HeavySnowFall
+import com.androidgeek.weather.features.weather.domain.model.WeatherType.HeavySnowShowers
+import com.androidgeek.weather.features.weather.domain.model.WeatherType.LightDrizzle
+import com.androidgeek.weather.features.weather.domain.model.WeatherType.LightFreezingDrizzle
+import com.androidgeek.weather.features.weather.domain.model.WeatherType.LightFreezingRain
+import com.androidgeek.weather.features.weather.domain.model.WeatherType.MainlyClear
+import com.androidgeek.weather.features.weather.domain.model.WeatherType.ModerateDrizzle
+import com.androidgeek.weather.features.weather.domain.model.WeatherType.ModerateRain
+import com.androidgeek.weather.features.weather.domain.model.WeatherType.ModerateRainShowers
+import com.androidgeek.weather.features.weather.domain.model.WeatherType.ModerateSnowFall
+import com.androidgeek.weather.features.weather.domain.model.WeatherType.ModerateThunderstorm
+import com.androidgeek.weather.features.weather.domain.model.WeatherType.Overcast
+import com.androidgeek.weather.features.weather.domain.model.WeatherType.PartlyCloudy
+import com.androidgeek.weather.features.weather.domain.model.WeatherType.SlightHailThunderstorm
+import com.androidgeek.weather.features.weather.domain.model.WeatherType.SlightRain
+import com.androidgeek.weather.features.weather.domain.model.WeatherType.SlightRainShowers
+import com.androidgeek.weather.features.weather.domain.model.WeatherType.SlightSnowFall
+import com.androidgeek.weather.features.weather.domain.model.WeatherType.SlightSnowShowers
+import com.androidgeek.weather.features.weather.domain.model.WeatherType.SnowGrains
+import com.androidgeek.weather.features.weather.domain.model.WeatherType.ViolentRainShowers
 import com.google.common.truth.Truth.assertThat
 import org.junit.jupiter.api.Test
 
@@ -10,27 +39,36 @@ internal class WeatherTypeTest {
 
     @Test
     fun fromWMO_code0_returnsClearSky() {
-        fromWMO(0).assertValue(
+        assertThat(fromWMO(0)).isEqualTo(ClearSky)
+    }
+
+    @Test
+    fun testClearSky() {
+        ClearSky.assertValue(
             weatherDesc = "Clear sky",
             iconRes = R.drawable.ic_sunny
         )
     }
 
     @Test
-    fun fromWMO_unsupportedCode_returnsDefaultClearSky() {
+    fun fromWMO_unsupportedCode_returnsDefault() {
         val unsupportedCode = Int.MIN_VALUE
-        val defaultWeatherType = fromWMO(unsupportedCode)
+        assertThat(fromWMO(unsupportedCode)).isEqualTo(DEFAULT)
+    }
 
-        assertThat(defaultWeatherType).isEqualTo(WeatherType.DEFAULT)
-        defaultWeatherType.assertValue(
-            weatherDesc = "Clear sky",
-            iconRes = R.drawable.ic_sunny
-        )
+    @Test
+    fun testDefaultIsEqualToClearSky() {
+        assertThat(DEFAULT).isEqualTo(ClearSky)
     }
 
     @Test
     fun fromWMO_code1_returnsMainlyClear() {
-        fromWMO(1).assertValue(
+        assertThat(fromWMO(1)).isEqualTo(MainlyClear)
+    }
+
+    @Test
+    fun testMainlyClear() {
+        MainlyClear.assertValue(
             weatherDesc = "Mainly clear",
             iconRes = R.drawable.ic_cloudy
         )
@@ -38,7 +76,12 @@ internal class WeatherTypeTest {
 
     @Test
     fun fromWMO_code2_returnsPartlyCloudy() {
-        fromWMO(2).assertValue(
+        assertThat(fromWMO(2)).isEqualTo(PartlyCloudy)
+    }
+
+    @Test
+    fun testPartlyCloudy() {
+        PartlyCloudy.assertValue(
             weatherDesc = "Partly cloudy",
             iconRes = R.drawable.ic_cloudy
         )
@@ -46,7 +89,12 @@ internal class WeatherTypeTest {
 
     @Test
     fun fromWMO_code3_returnsOvercast() {
-        fromWMO(3).assertValue(
+        assertThat(fromWMO(3)).isEqualTo(Overcast)
+    }
+
+    @Test
+    fun testOvercast() {
+        Overcast.assertValue(
             weatherDesc = "Overcast",
             iconRes = R.drawable.ic_cloudy
         )
@@ -54,7 +102,12 @@ internal class WeatherTypeTest {
 
     @Test
     fun fromWMO_code45_returnsFoggy() {
-        fromWMO(45).assertValue(
+        assertThat(fromWMO(45)).isEqualTo(Foggy)
+    }
+
+    @Test
+    fun testFoggy() {
+        Foggy.assertValue(
             weatherDesc = "Foggy",
             iconRes = R.drawable.ic_very_cloudy
         )
@@ -62,7 +115,12 @@ internal class WeatherTypeTest {
 
     @Test
     fun fromWMO_code48_returnsDepositingRimeFog() {
-        fromWMO(48).assertValue(
+        assertThat(fromWMO(48)).isEqualTo(DepositingRimeFog)
+    }
+
+    @Test
+    fun testDepositingRimeFog() {
+        DepositingRimeFog.assertValue(
             weatherDesc = "Depositing rime fog",
             iconRes = R.drawable.ic_very_cloudy
         )
@@ -70,7 +128,12 @@ internal class WeatherTypeTest {
 
     @Test
     fun fromWMO_code51_returnsLightDrizzle() {
-        fromWMO(51).assertValue(
+        assertThat(fromWMO(51)).isEqualTo(LightDrizzle)
+    }
+
+    @Test
+    fun testLightDrizzle() {
+        LightDrizzle.assertValue(
             weatherDesc = "Light drizzle",
             iconRes = R.drawable.ic_rainshower
         )
@@ -78,7 +141,12 @@ internal class WeatherTypeTest {
 
     @Test
     fun fromWMO_code53_returnsModerateDrizzle() {
-        fromWMO(53).assertValue(
+        assertThat(fromWMO(53)).isEqualTo(ModerateDrizzle)
+    }
+
+    @Test
+    fun testModerateDrizzle() {
+        ModerateDrizzle.assertValue(
             weatherDesc = "Moderate drizzle",
             iconRes = R.drawable.ic_rainshower
         )
@@ -86,7 +154,12 @@ internal class WeatherTypeTest {
 
     @Test
     fun fromWMO_code55_returnsDenseDrizzle() {
-        fromWMO(55).assertValue(
+        assertThat(fromWMO(55)).isEqualTo(DenseDrizzle)
+    }
+
+    @Test
+    fun testDenseDrizzle() {
+        DenseDrizzle.assertValue(
             weatherDesc = "Dense drizzle",
             iconRes = R.drawable.ic_rainshower
         )
@@ -94,7 +167,12 @@ internal class WeatherTypeTest {
 
     @Test
     fun fromWMO_code56_returnsLightFreezingDrizzle() {
-        fromWMO(56).assertValue(
+        assertThat(fromWMO(56)).isEqualTo(LightFreezingDrizzle)
+    }
+
+    @Test
+    fun testLightFreezingDrizzle() {
+        LightFreezingDrizzle.assertValue(
             weatherDesc = "Light freezing drizzle",
             iconRes = R.drawable.ic_snowyrainy
         )
@@ -102,7 +180,12 @@ internal class WeatherTypeTest {
 
     @Test
     fun fromWMO_code57_returnsDenseFreezingDrizzle() {
-        fromWMO(57).assertValue(
+        assertThat(fromWMO(57)).isEqualTo(DenseFreezingDrizzle)
+    }
+
+    @Test
+    fun testDenseFreezingDrizzle() {
+        DenseFreezingDrizzle.assertValue(
             weatherDesc = "Dense freezing drizzle",
             iconRes = R.drawable.ic_snowyrainy
         )
@@ -110,7 +193,12 @@ internal class WeatherTypeTest {
 
     @Test
     fun fromWMO_code61_returnsSlightRain() {
-        fromWMO(61).assertValue(
+        assertThat(fromWMO(61)).isEqualTo(SlightRain)
+    }
+
+    @Test
+    fun testSlightRain() {
+        SlightRain.assertValue(
             weatherDesc = "Slight rain",
             iconRes = R.drawable.ic_rainy
         )
@@ -118,7 +206,12 @@ internal class WeatherTypeTest {
 
     @Test
     fun fromWMO_code63_returnsModerateRain() {
-        fromWMO(63).assertValue(
+        assertThat(fromWMO(63)).isEqualTo(ModerateRain)
+    }
+
+    @Test
+    fun testModerateRain() {
+        ModerateRain.assertValue(
             weatherDesc = "Moderate rain",
             iconRes = R.drawable.ic_rainy
         )
@@ -126,7 +219,12 @@ internal class WeatherTypeTest {
 
     @Test
     fun fromWMO_code65_returnsHeavyRain() {
-        fromWMO(65).assertValue(
+        assertThat(fromWMO(65)).isEqualTo(HeavyRain)
+    }
+
+    @Test
+    fun testHeavyRain() {
+        HeavyRain.assertValue(
             weatherDesc = "Heavy rain",
             iconRes = R.drawable.ic_rainy
         )
@@ -134,72 +232,184 @@ internal class WeatherTypeTest {
 
     @Test
     fun fromWMO_code66_returnsLightFreezingRain() {
-        TODO()
+        assertThat(fromWMO(66)).isEqualTo(LightFreezingRain)
+    }
+
+    @Test
+    fun testLightFreezingRain() {
+        LightFreezingRain.assertValue(
+            weatherDesc = "Light freezing rain",
+            iconRes = R.drawable.ic_snowyrainy
+        )
     }
 
     @Test
     fun fromWMO_code67_returnsHeavyFreezingRain() {
-        TODO()
+        assertThat(fromWMO(67)).isEqualTo(HeavyFreezingRain)
+    }
+
+    @Test
+    fun testHeavyFreezingRain() {
+        HeavyFreezingRain.assertValue(
+            weatherDesc = "Heavy freezing rain",
+            iconRes = R.drawable.ic_snowyrainy
+        )
     }
 
     @Test
     fun fromWMO_code71_returnsSlightSnowFall() {
-        TODO()
+        assertThat(fromWMO(71)).isEqualTo(SlightSnowFall)
+    }
+
+    @Test
+    fun testSlightSnowFall() {
+        SlightSnowFall.assertValue(
+            weatherDesc = "Slight snow fall",
+            iconRes = R.drawable.ic_snowy
+        )
     }
 
     @Test
     fun fromWMO_code73_returnsModerateSnowFall() {
-        TODO()
+        assertThat(fromWMO(73)).isEqualTo(ModerateSnowFall)
+    }
+
+    @Test
+    fun testModerateSnowFall() {
+        ModerateSnowFall.assertValue(
+            weatherDesc = "Moderate snow fall",
+            iconRes = R.drawable.ic_heavysnow
+        )
     }
 
     @Test
     fun fromWMO_code75_returnsHeavySnowFall() {
-        TODO()
+        assertThat(fromWMO(75)).isEqualTo(HeavySnowFall)
+    }
+
+    @Test
+    fun testHeavySnowFall() {
+        HeavySnowFall.assertValue(
+            weatherDesc = "Heavy snow fall",
+            iconRes = R.drawable.ic_heavysnow
+        )
     }
 
     @Test
     fun fromWMO_code77_returnsSnowGrains() {
-        TODO()
+        assertThat(fromWMO(77)).isEqualTo(SnowGrains)
+    }
+
+    @Test
+    fun testSnowGrains() {
+        SnowGrains.assertValue(
+            weatherDesc = "Snow grains",
+            iconRes = R.drawable.ic_heavysnow
+        )
     }
 
     @Test
     fun fromWMO_code80_returnsSlightRainShowers() {
-        TODO()
+        assertThat(fromWMO(80)).isEqualTo(SlightRainShowers)
+    }
+
+    @Test
+    fun testSlightRainShowers() {
+        SlightRainShowers.assertValue(
+            weatherDesc = "Slight rain showers",
+            iconRes = R.drawable.ic_rainshower
+        )
     }
 
     @Test
     fun fromWMO_code81_returnsModerateRainShowers() {
-        TODO()
+        assertThat(fromWMO(81)).isEqualTo(ModerateRainShowers)
+    }
+
+    @Test
+    fun testModerateRainShowers() {
+        ModerateRainShowers.assertValue(
+            weatherDesc = "Moderate rain showers",
+            iconRes = R.drawable.ic_rainshower
+        )
     }
 
     @Test
     fun fromWMO_code82_returnsViolentRainShowers() {
-        TODO()
+        assertThat(fromWMO(82)).isEqualTo(ViolentRainShowers)
+    }
+
+    @Test
+    fun testViolentRainShowers() {
+        ViolentRainShowers.assertValue(
+            weatherDesc = "Violent rain showers",
+            iconRes = R.drawable.ic_rainshower
+        )
     }
 
     @Test
     fun fromWMO_code85_returnsSlightSnowShowers() {
-        TODO()
+        assertThat(fromWMO(85)).isEqualTo(SlightSnowShowers)
+    }
+
+    @Test
+    fun testSlightSnowShowers() {
+        SlightSnowShowers.assertValue(
+            weatherDesc = "Slight snow showers",
+            iconRes = R.drawable.ic_snowy
+        )
     }
 
     @Test
     fun fromWMO_code86_returnsHeavySnowShowers() {
-        TODO()
+        assertThat(fromWMO(86)).isEqualTo(HeavySnowShowers)
+    }
+
+    @Test
+    fun testHeavySnowShowers() {
+        HeavySnowShowers.assertValue(
+            weatherDesc = "Heavy snow showers",
+            iconRes = R.drawable.ic_snowy
+        )
     }
 
     @Test
     fun fromWMO_code95_returnsModerateThunderstorm() {
-        TODO()
+        assertThat(fromWMO(95)).isEqualTo(ModerateThunderstorm)
+    }
+
+    @Test
+    fun testModerateThunderstorm() {
+        ModerateThunderstorm.assertValue(
+            weatherDesc = "Moderate thunderstorm",
+            iconRes = R.drawable.ic_thunder
+        )
     }
 
     @Test
     fun fromWMO_code96_returnsSlightHailThunderstorm() {
-        TODO()
+        assertThat(fromWMO(96)).isEqualTo(SlightHailThunderstorm)
+    }
+
+    @Test
+    fun testSlightHailThunderstorm() {
+        SlightHailThunderstorm.assertValue(
+            weatherDesc = "Thunderstorm with slight hail",
+            iconRes = R.drawable.ic_rainythunder
+        )
     }
 
     @Test
     fun fromWMO_code99_returnsHeavyHailThunderstorm() {
-        TODO()
+        assertThat(fromWMO(99)).isEqualTo(HeavyHailThunderstorm)
+    }
+
+    @Test
+    fun testHeavyHailThunderstorm() {
+        HeavyHailThunderstorm.assertValue(
+            "Thunderstorm with heavy hail",
+            R.drawable.ic_rainythunder
+        )
     }
 
     private fun WeatherType.assertValue(weatherDesc: String, @DrawableRes iconRes: Int) {
